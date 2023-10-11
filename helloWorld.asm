7eb917957541d7a493d09d882d8676ec2f74839d
Luke Green CS2019 <luke.green.2019@uni.strath.ac.uk>
2023-10-12 00:33:33 +0100

    Initial Commit

---
+++ helloWorld.asm
@@ -0,0 +1,21 @@
+section .bss
+    ; variables
+
+section .data
+    ;consts
+    hello: db "Hello World!", 10
+    helloLen: equ $-hello
+
+section .text
+    global _start
+
+    _start:
+        mov rax,1
+        mov rdi,1
+        mov rsi,hello
+        mov rdx, helloLen
+        syscall
+
+        mov rax,60
+        mov rdi,0
+        syscall
